3a25ff159d725883afec43f21276aad040ad74c8
nicogorlo <gorlon@ethz.ch>
2024-03-05 01:19:14 +0100

    added docstrings

--- plr_exercise/models/cnn.py
+++ plr_exercise/models/cnn.py
@@ -5,6 +5,9 @@ import torch.nn.functional as F
 
 
 class Net(nn.Module):
+    """
+    CNN Module, convolutional layers with dropout
+    """
     def __init__(self):
 
         super(Net, self).__init__()
