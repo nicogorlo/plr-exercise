4ed4bbb90ea0e2651eaa7f441c5677dde68c944e
nicogorlo <gorlon@ethz.ch>
2024-03-05 01:13:10 +0100

    added optuna

--- scripts/train.py
+++ scripts/train.py
@@ -7,9 +7,10 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 from plr_exercise.models.cnn import Net
+import numpy as np
 
 import wandb
-
+import optuna
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
@@ -51,7 +52,7 @@ def test(model, device, test_loader, epoch):
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
-
+    
     wandb.log({"test_loss": test_loss})
 
     print(
@@ -59,6 +60,7 @@ def test(model, device, test_loader, epoch):
             test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
         )
     )
+    return test_loss
 
 
 def main():
@@ -83,6 +85,7 @@ def main():
         metavar="N",
         help="how many batches to wait before logging training status",
     )
+    parser.add_argument("--optimize", type=bool, default=False, help="Optimize the model with optuna")
     parser.add_argument("--save-model", action="store_true", default=False, help="For Saving the current Model")
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
@@ -112,26 +115,55 @@ def main():
         cuda_kwargs = {"num_workers": 1, "pin_memory": True, "shuffle": True}
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
-
+    
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
     dataset1 = datasets.MNIST("../data", train=True, download=True, transform=transform)
     dataset2 = datasets.MNIST("../data", train=False, transform=transform)
     train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
-    model = Net().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    if args.optimize:
+        model = Net().to(device)
+        optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
-    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    for epoch in range(args.epochs):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader, epoch)
-        scheduler.step()
+        def objective(trial):
+            lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
+            optimizer = optim.Adam(model.parameters(), lr=lr)
+            best_test_loss = np.inf
+            best_epoch = 0
+
+            for epoch in range(args.epochs):
+                train(args, model, device, train_loader, optimizer, epoch)
+                test_loss = test(model, device, test_loader, epoch)
+                if test_loss < best_test_loss:
+                    best_test_loss = test_loss
+                    best_epoch = epoch
+
+            trial.set_user_attr("best_epoch", best_epoch)
+
+            return best_test_loss
         
+        scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+        study = optuna.create_study(direction="minimize")
+        study.optimize(objective, n_trials=5)
+        best_trial = study.best_trial
+        print("best params: ", best_trial.params)
+        print("best test loss: ", best_trial.value)
+        print("Best epoch: ", best_trial.user_attrs['best_epoch'])
+    else: 
+        model = Net().to(device)
+        optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+        scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+        for epoch in range(args.epochs):
+            train(args, model, device, train_loader, optimizer, epoch)
+            test_loss = test(model, device, test_loader, epoch)
+            scheduler.step()
 
     if args.save_model:
         torch.save(model.state_dict(), "mnist_cnn.pt")
 
+    run.finish()
 
 if __name__ == "__main__":
     main()
